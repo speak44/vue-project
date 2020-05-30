c94aecd1e45911e740b3bda1e7f3656e0d693137
chensssss <cherrychen1@shunshunliuxue.com>
2020-05-30 16:53:46 +0800

    plugins

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './user';
-// console.log(user,'user')
+import persist from './plugins/persist';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     user
   },
+  plugins:[persist],
   strict: true  // 严格模式，防止用户手动去修改状态
+
 })
--- src/store/plugins/persist.js
+++ src/store/plugins/persist.js
@@ -1,6 +1,18 @@
-// export default store => {
-//   // sotre 初始化的时候，将存储在localstore中的状态还原
-//   // if(){
-
-//   // }
-// }
+export default store => {
+  // sotre 初始化的时候，将存储在localstore中的状态还原
+  if(localStorage) {
+   const user =JSON.parse(localStorage.getItem('user'))
+   if(user){
+      store.commit('user/login',user.username)
+   }
+  }
+  // 如果用户状态发生改变， 自动存入localStore里面
+  store.subscribe((mutation, state)=>{
+    if(mutation.type==="user/login") {
+      const user = JSON.stringify(state.user)
+      localStorage.setItem('user',user)
+    }else if(mutation.type==="user/logout") {
+      localStorage.removeItem('user')
+    }
+  })
+}
